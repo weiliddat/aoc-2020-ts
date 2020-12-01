b7a13c8aab40505bd29c7cdd76af931db79d3748
Chia Wei <ongchiawei@gmail.com>
2020-12-02 00:01:17 +0100

    day 1 prettify refactor

--- 1/solution.js
+++ 1/solution.js
@@ -1,5 +1,4 @@
 "use strict";
-var _a, _b;
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs_1 = require("fs");
 var lodash_1 = require("lodash");
@@ -8,14 +7,23 @@ var numbers = input.split('\n').map(function (v) { return parseInt(v, 10); });
 function cross2(numbers) {
     return lodash_1.flatMap(numbers, function (a) { return lodash_1.map(numbers, function (b) { return [a, b]; }); });
 }
-var firstCrossProduct = cross2(numbers);
-var first2020 = lodash_1.filter(firstCrossProduct, function (v) { return lodash_1.sum(v) === 2020; });
-var firstResult = (_a = lodash_1.head(first2020)) === null || _a === void 0 ? void 0 : _a.reduce(function (a, b) { return a * b; });
-console.log(firstResult);
 function cross3(numbers) {
-    return lodash_1.flatMap(numbers, function (a) { return lodash_1.flatMap(numbers, function (b) { return lodash_1.map(numbers, function (c) { return [a, b, c]; }); }); });
+    return lodash_1.flatMap(numbers, function (a) {
+        return lodash_1.flatMap(numbers, function (b) { return lodash_1.map(numbers, function (c) { return [a, b, c]; }); });
+    });
 }
+function get2020Items(numbers) {
+    var sumsTo2020 = lodash_1.filter(numbers, function (v) { return lodash_1.sum(v) === 2020; });
+    var first = lodash_1.head(sumsTo2020) || [];
+    return first;
+}
+function getProduct(numbers) {
+    return numbers.reduce(function (a, b) { return a * b; });
+}
+var getResult = function (list) { return getProduct(get2020Items(list)); };
+var firstCrossProduct = cross2(numbers);
+var firstResult = getResult(firstCrossProduct);
+console.log(firstResult);
 var secondCrossProduct = cross3(numbers);
-var second2020 = lodash_1.filter(secondCrossProduct, function (v) { return lodash_1.sum(v) === 2020; });
-var secondResult = (_b = lodash_1.head(second2020)) === null || _b === void 0 ? void 0 : _b.reduce(function (a, b) { return a * b; });
+var secondResult = getResult(secondCrossProduct);
 console.log(secondResult);
--- 1/solution.ts
+++ 1/solution.ts
@@ -6,25 +6,35 @@ const input = readFileSync('1/input.txt', 'utf-8');
 const numbers = input.split('\n').map((v) => parseInt(v, 10));
 
 function cross2(numbers: number[]) {
-    return flatMap(numbers, (a) => map(numbers, (b) => [a, b]));
+  return flatMap(numbers, (a) => map(numbers, (b) => [a, b]));
 }
+
+function cross3(numbers: number[]) {
+  return flatMap(numbers, (a) =>
+    flatMap(numbers, (b) => map(numbers, (c) => [a, b, c])),
+  );
+}
+
+function get2020Items(numbers: number[][]) {
+  const sumsTo2020 = filter(numbers, (v) => sum(v) === 2020);
+  const first = head(sumsTo2020) || [];
+  return first;
+}
+
+function getProduct(numbers: number[]) {
+  return numbers.reduce((a, b) => a * b);
+}
+
+const getResult = (list: number[][]) => getProduct(get2020Items(list));
 
 const firstCrossProduct = cross2(numbers);
 
-const first2020 = filter(firstCrossProduct, (v) => sum(v) === 2020);
-
-const firstResult = head(first2020)?.reduce((a, b) => a * b);
+const firstResult = getResult(firstCrossProduct);
 
 console.log(firstResult);
 
-function cross3(numbers: number[]) {
-    return flatMap(numbers, (a) => flatMap(numbers, (b) => map(numbers, (c) => [a, b, c])));
-}
-
 const secondCrossProduct = cross3(numbers);
 
-const second2020 = filter(secondCrossProduct, (v) => sum(v) === 2020);
-
-const secondResult = head(second2020)?.reduce((a, b) => a * b);
+const secondResult = getResult(secondCrossProduct);
 
 console.log(secondResult);
