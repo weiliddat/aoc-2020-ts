df3747d2799756c399f111029af9ef1e12d24a5f
Chia Wei <ongchiawei@gmail.com>
2020-12-04 06:48:22 +0100

    day 4 part 1 solution

--- 4/solution.ts
+++ 4/solution.ts
@@ -3,3 +3,29 @@ import { join } from 'path';
 import _ from 'lodash';
 
 const input = readFileSync(join(__dirname, 'input.txt'), 'utf-8');
+
+const passports = _.split(input, '\n\n')
+  .map((ps) => _.split(ps, /\s/))
+  .map((ps) =>
+    _.reduce(
+      ps,
+      (prev, curr) => {
+        const [key, value] = _.split(curr, ':');
+        return {
+          ...prev,
+          [key]: value,
+        };
+      },
+      {},
+    ),
+  );
+
+const requiredFields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
+
+const passportMissingFields = _.map(passports, (p) =>
+  _.reject(requiredFields, (f) => _.includes(_.keys(p), f)),
+);
+
+const validPassports = _.filter(passportMissingFields, _.isEmpty);
+
+console.log(validPassports.length);
