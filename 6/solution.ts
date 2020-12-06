9963b0f53fbdf9f906e969dfc7284c6de84748d8
Chia Wei <ongchiawei@gmail.com>
2020-12-06 07:12:43 +0100

    day 6 part 1 solution

--- 6/solution.ts
+++ 6/solution.ts
@@ -3,3 +3,24 @@ import { join } from 'path';
 import _ from 'lodash';
 
 const input = readFileSync(join(__dirname, 'input.txt'), 'utf-8');
+
+const answersByGroup = _.split(input, '\n\n').map((v) => {
+  const memberAnswers = _.split(v, '\n').map((ans) => ({
+    string: ans,
+    list: _.split(ans, ''),
+  }));
+
+  const deduplicated = _.union(_.flatMap(memberAnswers, 'list'));
+
+  return {
+    memberAnswers,
+    deduplicated,
+    count: deduplicated.length,
+  };
+});
+
+console.dir(answersByGroup, { depth: null });
+
+const sumOfUniqueAnswers = _.sumBy(answersByGroup, 'count');
+
+console.log(sumOfUniqueAnswers);
