4a0ead93087516df3a5d6e25191a88b21e92567b
Chia Wei <ongchiawei@gmail.com>
2020-12-09 08:14:47 +0100

    day 9 part 1 solved

--- 9/solution.ts
+++ 9/solution.ts
@@ -3,3 +3,23 @@ import { join } from 'path';
 import _ from 'lodash';
 
 const input = readFileSync(join(__dirname, 'input.txt'), 'utf-8');
+
+const listOfNumbers = _.split(input, '\n').map((a) => parseInt(a, 10));
+
+function cross2Unique(numbers: number[]) {
+  return _.flatMap(numbers, (a) =>
+    _.map(numbers, (b): [number, number] => [a, b]),
+  ).filter((a) => a[0] !== a[1]);
+}
+
+const checkNumbers = (l: number[], ignore: number) =>
+  _.chain(l)
+    .find((a, i) => {
+      if (i < ignore) return false;
+      const prev = _.slice(l, i - ignore, i);
+      const sums = cross2Unique(prev).map(_.sum);
+      return !_.includes(sums, a);
+    })
+    .value();
+
+console.log(checkNumbers(listOfNumbers, 25));
